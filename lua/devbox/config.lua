rbelem/lazyvim-starter

feat: add devbox.nvim plugin for devbox env integration
Local plugin that detects devbox.json in project roots, runs
devbox shellenv, and injects env vars into vim.env so LSP servers
(jdtls, etc.) find mvn/java/go without global installs.

- lua/devbox/  plugin module (init.lua, config.lua, lsp.lua)
- lua/plugins/devbox.lua  LazyVim spec

lua/devbox/config.lua
@@ -1,0 +1,41 @@
+---@class devbox.Config
+---@field auto_activate boolean
+---@field update_env boolean
+---@field strategy string  -- "sync" | "async"
+---@field silent boolean
+---@field devbox_path string
+---@field lsp? { inject_env: boolean }
+---@field exclude_env? string[]
+
+local M = {}
+
+---@type devbox.Config
+M.defaults = {
+  auto_activate = true,
+  update_env = true,
+  strategy = "sync",
+  silent = false,
+  devbox_path = "devbox",
+  lsp = { inject_env = true },
+  exclude_env = {
+    "^ATUIN_",
+    "^BLE_",
+    "_PREEXEC_",
+    "^BASH_",
+    "^SHELL",
+    "^TERM",
+    "^LS_COLORS",
+    "^HIST",
+    "^PROMPT",
+  },
+}
+
+---@type devbox.Config
+M.options = {}
+
+---@param opts? devbox.Config
+function M.setup(opts)
+  M.options = vim.tbl_deep_extend("force", {}, M.defaults, opts or {})
+end
+
+return M

rbelem/devbox.nvim

lua/devbox/config.lua
@@ -1,0 +1,41 @@
+---@class devbox.Config
+---@field auto_activate boolean
+---@field update_env boolean
+---@field strategy string  -- "async" (default) | "sync"
+---@field silent boolean
+---@field devbox_path string
+---@field lsp? { inject_env: boolean }
+---@field exclude_env? string[]
+
+local M = {}
+
+---@type devbox.Config
+M.defaults = {
+  auto_activate = true,
+  update_env = true,
+  strategy = "async",
+  silent = false,
+  devbox_path = "devbox",
+  lsp = { inject_env = true },
+  exclude_env = {
+    "^ATUIN_",
+    "^BLE_",
+    "_PREEXEC_",
+    "^BASH_",
+    "^SHELL",
+    "^TERM",
+    "^LS_COLORS",
+    "^HIST",
+    "^PROMPT",
+  },
+}
+
+---@type devbox.Config
+M.options = {}
+
+---@param opts? devbox.Config
+function M.setup(opts)
+  M.options = vim.tbl_deep_extend("force", {}, M.defaults, opts or {})
+end
+
+return M